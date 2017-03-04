127b1abe063f45d10ba59012a4d9ad8f84916c21
apethoud <andrewpethoud@gmail.com>
2017-03-03 20:45:51 -0800

    finished randomize and victory functionality

--- sliderpuzzle/js/sliderpuzzle.js
+++ sliderpuzzle/js/sliderpuzzle.js
@@ -1,6 +1,8 @@
+var victoryText = document.querySelector('#victory-text');
+var newGameBtn = document.querySelector('#new-game-btn');
 var tileGrid = document.querySelector('.tile-grid');
-var victoryText = document.querySelector('.victory');
 var numberList = ['1','2','3','4','5','6','7','8'];
+var vict = false;
 
 var squareOne = document.querySelector('#s1');
 var squareTwo = document.querySelector('#s2');
@@ -30,7 +32,18 @@ function randomizeOrder(){
     numberList[i] = numberList[j];
     numberList[j] = temp;
   }
+
+  squareOne.firstElementChild.textContent = numberList[0];
+  squareTwo.firstElementChild.textContent = numberList[1];
+  squareThree.firstElementChild.textContent = numberList[2];
+  squareFour.firstElementChild.textContent = numberList[3];
+  squareFive.firstElementChild.textContent = numberList[4];
+  squareSix.firstElementChild.textContent = numberList[5];
+  squareSeven.firstElementChild.textContent = numberList[6];
+  squareEight.firstElementChild.textContent = numberList[7];
 }
+
+randomizeOrder();
 
 tileGrid.addEventListener('click', function(e){
   // when you click a square:
@@ -38,116 +51,137 @@ tileGrid.addEventListener('click', function(e){
     // check if one of that square's horizontal and vertical neighbors is empty. If not, do nothing. If so:
   var clickedTile = e.target;
 
-  if(clickedTile === squareOne){
-    if(squareTwo.classList.contains('invisible')){
-      switchTiles(squareOne,squareTwo);
+  if(!vict){
+    if(clickedTile === squareOne){
+      if(squareTwo.classList.contains('invisible')){
+        switchTiles(squareOne,squareTwo);
+      }
+      if(squareFour.classList.contains('invisible')){
+        switchTiles(squareOne,squareFour);
+      }
     }
-    if(squareFour.classList.contains('invisible')){
-      switchTiles(squareOne,squareFour);
+
+    if(clickedTile === squareTwo){
+      if(squareOne.classList.contains('invisible')){
+        switchTiles(squareTwo,squareOne);
+      }
+      if(squareThree.classList.contains('invisible')){
+        switchTiles(squareTwo,squareThree);
+      }
+      if(squareFive.classList.contains('invisible')){
+        switchTiles(squareTwo,squareFive);
+      }
+    }
+
+    if(clickedTile === squareThree){
+      if(squareTwo.classList.contains('invisible')){
+        switchTiles(squareThree,squareTwo);
+      }
+      if(squareSix.classList.contains('invisible')){
+        switchTiles(squareThree,squareSix);
+      }
+    }
+
+    if(clickedTile === squareFour){
+      if(squareOne.classList.contains('invisible')){
+        switchTiles(squareFour,squareOne);
+      }
+      if(squareFive.classList.contains('invisible')){
+        switchTiles(squareFour,squareFive);
+      }
+      if(squareSeven.classList.contains('invisible')){
+        switchTiles(squareFour,squareSeven);
+      }
+    }
+
+    if(clickedTile === squareFive){
+      if(squareTwo.classList.contains('invisible')){
+        switchTiles(squareFive,squareTwo);
+      }
+      if(squareFour.classList.contains('invisible')){
+        switchTiles(squareFive,squareFour);
+      }
+      if(squareSix.classList.contains('invisible')){
+        switchTiles(squareFive,squareSix);
+      }
+      if(squareEight.classList.contains('invisible')){
+        switchTiles(squareFive,squareEight);
+      }
+    }
+
+    if(clickedTile === squareSix){
+      if(squareThree.classList.contains('invisible')){
+        switchTiles(squareSix,squareThree);
+      }
+      if(squareFive.classList.contains('invisible')){
+        switchTiles(squareSix,squareFive);
+      }
+      if(squareNine.classList.contains('invisible')){
+        switchTiles(squareSix,squareNine);
+      }
+    }
+
+    if(clickedTile === squareSeven){
+      if(squareFour.classList.contains('invisible')){
+        switchTiles(squareSeven,squareFour);
+      }
+      if(squareEight.classList.contains('invisible')){
+        switchTiles(squareSeven,squareEight);
+      }
+    }
+
+    if(clickedTile === squareEight){
+      if(squareFive.classList.contains('invisible')){
+        switchTiles(squareEight,squareFive);
+      }
+      if(squareSeven.classList.contains('invisible')){
+        switchTiles(squareEight,squareSeven);
+      }
+      if(squareNine.classList.contains('invisible')){
+        switchTiles(squareEight,squareNine);
+      }
+    }
+
+    if(clickedTile === squareNine){
+      if(squareSix.classList.contains('invisible')){
+        switchTiles(squareNine,squareSix);
+      }
+      if(squareEight.classList.contains('invisible')){
+        switchTiles(squareNine,squareEight);
+      }
+    }
+
+  // Checks if the puzzle is solved
+    if(squareOne.firstElementChild.textContent == 1 &&
+       squareTwo.firstElementChild.textContent == 2 &&
+       squareThree.firstElementChild.textContent == 3 &&
+       squareFour.firstElementChild.textContent == 4 &&
+       squareFive.firstElementChild.textContent == 5 &&
+       squareSix.firstElementChild.textContent == 6 &&
+       squareSeven.firstElementChild.textContent == 7 &&
+       squareEight.firstElementChild.textContent == 8){
+      victoryText.classList.toggle('hidden');
+      newGameBtn.classList.add('glow');
+      vict = true;
     }
   }
+});
 
-  if(clickedTile === squareTwo){
-    if(squareOne.classList.contains('invisible')){
-      switchTiles(squareTwo,squareOne);
-    }
-    if(squareThree.classList.contains('invisible')){
-      switchTiles(squareTwo,squareThree);
-    }
-    if(squareFive.classList.contains('invisible')){
-      switchTiles(squareTwo,squareFive);
-    }
-  }
+newGameBtn.addEventListener('click', function(){
+  randomizeOrder();
+  squareNine.firstElementChild.textContent = "";
 
-  if(clickedTile === squareThree){
-    if(squareTwo.classList.contains('invisible')){
-      switchTiles(squareThree,squareTwo);
-    }
-    if(squareSix.classList.contains('invisible')){
-      switchTiles(squareThree,squareSix);
-    }
-  }
+  squareOne.classList.remove('invisible');
+  squareTwo.classList.remove('invisible');
+  squareThree.classList.remove('invisible');
+  squareFour.classList.remove('invisible');
+  squareFive.classList.remove('invisible');
+  squareSix.classList.remove('invisible');
+  squareSeven.classList.remove('invisible');
+  squareEight.classList.remove('invisible');
+  squareNine.classList.add('invisible');
 
-  if(clickedTile === squareFour){
-    if(squareOne.classList.contains('invisible')){
-      switchTiles(squareFour,squareOne);
-    }
-    if(squareFive.classList.contains('invisible')){
-      switchTiles(squareFour,squareFive);
-    }
-    if(squareSeven.classList.contains('invisible')){
-      switchTiles(squareFour,squareSeven);
-    }
-  }
-
-  if(clickedTile === squareFive){
-    if(squareTwo.classList.contains('invisible')){
-      switchTiles(squareFive,squareTwo);
-    }
-    if(squareFour.classList.contains('invisible')){
-      switchTiles(squareFive,squareFour);
-    }
-    if(squareSix.classList.contains('invisible')){
-      switchTiles(squareFive,squareSix);
-    }
-    if(squareEight.classList.contains('invisible')){
-      switchTiles(squareFive,squareEight);
-    }
-  }
-
-  if(clickedTile === squareSix){
-    if(squareThree.classList.contains('invisible')){
-      switchTiles(squareSix,squareThree);
-    }
-    if(squareFive.classList.contains('invisible')){
-      switchTiles(squareSix,squareFive);
-    }
-    if(squareNine.classList.contains('invisible')){
-      switchTiles(squareSix,squareNine);
-    }
-  }
-
-  if(clickedTile === squareSeven){
-    if(squareFour.classList.contains('invisible')){
-      switchTiles(squareSeven,squareFour);
-    }
-    if(squareEight.classList.contains('invisible')){
-      switchTiles(squareSeven,squareEight);
-    }
-  }
-
-  if(clickedTile === squareEight){
-    if(squareFive.classList.contains('invisible')){
-      switchTiles(squareEight,squareFive);
-    }
-    if(squareSeven.classList.contains('invisible')){
-      switchTiles(squareEight,squareSeven);
-    }
-    if(squareNine.classList.contains('invisible')){
-      switchTiles(squareEight,squareNine);
-    }
-  }
-
-  if(clickedTile === squareNine){
-    if(squareSix.classList.contains('invisible')){
-      switchTiles(squareNine,squareSix);
-    }
-    if(squareEight.classList.contains('invisible')){
-      switchTiles(squareNine,squareEight);
-    }
-  }
-
-      // check to see if all the tiles are in the correct order. If so, indicate the win. Also insert a reset button to reset the grid.
-
-  if(squareOne.firstElementChild.textContent == 1 &&
-     squareTwo.firstElementChild.textContent == 2 &&
-     squareThree.firstElementChild.textContent == 3 &&
-     squareFour.firstElementChild.textContent == 4 &&
-     squareFive.firstElementChild.textContent == 5 &&
-     squareSix.firstElementChild.textContent == 6 &&
-     squareSeven.firstElementChild.textContent == 7 &&
-     squareEight.firstElementChild.textContent == 8){
-    victoryText.classList.toggle('hidden');
-  }
-
+  victoryText.classList.add('hidden');
+  newGameBtn.classList.remove('glow');
+  vict = false;
 });
